b3a02febc5643729cf6c336b2fb554e10fac83a6
Tangue Muweh <atmuweh@gmail.com>
2020-05-23 22:46:18 +0000

    check for command line argument

---
+++ pset2/caesar/caesar.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, string argv[])
+{
+
+    //check for arguments and print respective message
+    if(argc != 2)
+    {
+        printf("Usage: ./caesar key \n");
+    }
+
+    //convert string to integer using atoi()
+
+
+    //prompt user to import plain text
+
+    //cipher plain text
+
+        //cipher one character to preserve the case
+
+        //preserve non alphanum characters
+
+
+
+
+}
