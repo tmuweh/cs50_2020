4e88d1e8f8cc094c3a3c8f10743b31f2ca309e8f
Tangue Muweh <atmuweh@gmail.com>
2020-05-26 03:55:28 +0000

    validate key for repeated character

--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <cytpe.h>
+#include <ctype.h>
 #include <cs50.h>
 #include <string.h>
 #include <stdlib.h>
@@ -15,27 +15,43 @@ int main(int argc, string argv[])
   }
 
   //check for length of argument
-  if (strlen(argv[i]) != 26)
+  if (strlen(argv[1]) != 26)
   {
     printf("Key must contain 26 characters.");
     return 1;
   }
 
-  //validate 26 key cipher 
+  //validate 26 key cipher
+
+  int letter[26]; //store counts of occurrences of individual letters 0 - 25
+
+  for(int i = 0; i < sizeof(letter)/sizeof(int); i++)
+    letter[0] = 0;
+
   for (int i = 0; i < strlen(argv[1]); i++) // loops through argumetn string
   {
+
     if (!isalpha(argv[1][i])) //for alpha character
     {
       printf("Usage: ./caesar key \n");
       return 1;
     }
-    if (!) //check for repeated char
+    //check for repeated char
+    if (isupper(argv[1][i]))
     {
-      
+      ++letter[argv[0][i] - 'A'];
     }
-    
-    
-    //if exits loop without return then... no non-numeric found
+    else
+    {
+      ++letter[argv[1][i] - 'a'];
+    }
+    //letter occured more than once
+    if(letter[i] > 1)
+    {
+      printf("Key must not contain repeated characters.");
+      return 1;
+    }
+    //if exits loop without return then... 26 Key accepted
   }
 
   //cipher text is case insensitive
@@ -50,6 +66,9 @@ int main(int argc, string argv[])
     //check availability and case
 
   //print cipher text
+
+  //print new line
+  printf("\n");
 
   return 0;
 }
