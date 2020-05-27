d268a6ec7865f2475c311df64bb830320a7ac856
Tangue Muweh <atmuweh@gmail.com>
2020-05-27 02:05:01 +0000

    validate key for repeated characters

--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -9,7 +9,7 @@ int main(int argc, string argv[])
   //check if user put in an argument
   if(argc != 2)
   {
-      printf("Usage: ./caesar key \n");
+      printf("Usage: ./caesar KEY \n");
 
       return 1;
   }
@@ -17,43 +17,46 @@ int main(int argc, string argv[])
   //check for length of argument
   if (strlen(argv[1]) != 26)
   {
-    printf("Key must contain 26 characters.");
+    printf("Key must contain 26 characters.\n");
     return 1;
   }
 
   //validate 26 key cipher
 
-  int letter[26]; //store counts of occurrences of individual letters 0 - 25
-
-  for(int i = 0; i < sizeof(letter)/sizeof(int); i++)
-    letter[0] = 0;
+  //store counts of occurrences of individual letters 0 - 25, initialize count to 0 per letter
+  int letter[26];
+  for (int i = 0; i < 26; i++){
+      letter[i] = 0;
+  }
 
   for (int i = 0; i < strlen(argv[1]); i++) // loops through argumetn string
   {
-
     if (!isalpha(argv[1][i])) //for alpha character
     {
-      printf("Usage: ./caesar key \n");
+      printf("Usage: ./caesar KEY \n");
       return 1;
     }
-    //check for repeated char
+    //count repeated char
     if (isupper(argv[1][i]))
     {
-      ++letter[argv[0][i] - 'A'];
+      ++letter[argv[1][i] - 'A'];
     }
-    else
+    if (islower(argv[1][i]))
     {
       ++letter[argv[1][i] - 'a'];
     }
-    //letter occured more than once
-    if(letter[i] > 1)
-    {
-      printf("Key must not contain repeated characters.");
-      return 1;
-    }
-    //if exits loop without return then... 26 Key accepted
   }
 
+  for (int i = 0; i < 26; i++)
+  {
+    //letter occured more than once
+    if (letter[i] > 1)
+    {
+      printf("Key must not contain repeated characters.\n");
+
+      return 1;
+    }
+  }
   //cipher text is case insensitive
 
 
