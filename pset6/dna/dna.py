fa427073464734e90db65d27eec56f44aac1b1db
Tangue Muweh <atmuweh@gmail.com>
2020-07-19 03:41:22 +0000

    take care of commandline arguments

---
+++ pset6/dna/dna.py
@@ -0,0 +1,27 @@
+# import command line module
+from sys import argv, exit
+
+# import csv file module
+from csv import reader, DictReader
+
+# import other helper module
+from cs50 import get_string
+
+# check for commandline arguments
+if len(argv) != 3:
+    print("Usage: python dna.py databases.csv sequences.txt")
+    exit(1)
+
+# open database and save content to memory
+
+
+# open sequence file and save to memory
+
+# get number of STRs from database
+
+# for each STRs in database search sequence for the longest occurence
+
+    # keep track of the repeats
+
+# compare repeats with STRs in the data base and print name of owner or not found
+
