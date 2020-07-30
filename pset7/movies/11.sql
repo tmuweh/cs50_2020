04a2e11f14441586098f956f87d26ab10cb5024b
Tangue Muweh <atmuweh@gmail.com>
2020-07-30 13:24:33 +0000

    get movies stared by bosemann

---
+++ pset7/movies/11.sql
@@ -0,0 +1,1 @@
+SELECT title FROM movies JOIN ratings ON movies.id = ratings.movie_id JOIN stars ON ratings.movie_id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE name = "Chadwick Boseman" ORDER BY rating DESC LIMIT 5;
