a518f1272fe50b8fd4784d19df42fa8fe999c27b
Tangue Muweh <atmuweh@gmail.com>
2020-08-01 15:33:20 +0000

    do a count on the rows return

--- pset7/movies/4.sql
+++ pset7/movies/4.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies WHERE id = (SELECT movie_id FROM ratings WHERE rating = 10.0);
+SELECT COUNT(title) FROM movies WHERE id IN (SELECT movie_id FROM ratings WHERE rating = 10.0);
