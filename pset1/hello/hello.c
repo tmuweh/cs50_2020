c359441b1787a45f17d559c676e172aa808bf0a1
Tangue Muweh <atmuweh@gmail.com>
2020-08-04 22:54:11 +0000

    style change

--- pset1/hello/hello.c
+++ pset1/hello/hello.c
@@ -3,10 +3,10 @@
 
 int main(void)
 {
-
-	string name = get_string("What is your name?\n"); //get input name from user
-
-	printf("Hello, %s!\n", name); //print hello plus the user name entered from user
+	//get input name from user
+	string name = get_string("What is your name?\n");
+	//print hello plus the user name entered from user
+	printf("Hello, %s!\n", name);
 
 	return 0;
 }
