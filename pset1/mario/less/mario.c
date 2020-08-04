980a93ac193d11bf14a4ed78f785a9d1312d1969
Tangue Muweh <atmuweh@gmail.com>
2020-08-04 22:51:55 +0000

    more styling on codes

--- pset1/mario/less/mario.c
+++ pset1/mario/less/mario.c
@@ -4,17 +4,19 @@
 int main(void)
 {
     int height = 0;
-    do{
+    do
+    {
         height = get_int("Enter a height: ");
 
-    }while(height < 1 || height > 8);
+    }
+    while(height < 1 || height > 8);
 
      for (int  row  =  0; row < height; row++)
     {
-        for(int column = 0; column < height; column++)
+        for (int column = 0; column < height; column++)
         {
             //print space or hash tag using formular nth row + space <= height - 1 print space else, print hash
-            if( (row + column) < height - 1)
+            if ( (row + column) < height - 1)
 
                 printf(" ");
             else
