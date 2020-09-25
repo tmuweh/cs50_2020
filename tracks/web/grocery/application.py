81bc56ce1746baae886c30a6fdf45a20948b6689
Tangue Muweh <atmuweh@gmail.com>
2020-09-25 08:16:58 +0000

    list of grocies

--- tracks/web/grocery/application.py
+++ tracks/web/grocery/application.py
@@ -1,11 +1,19 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
+#list of grocies
+grocies = []
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html" , grocies=grocies)
 
-@app.route("/add", methods=["get", "post"])
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    return render_template("add.html")
+    if request.method == "GET":
+        return render_template("add.html")
+    else:
+        name = request.form.get("name")
+        grocies.append(name)
+        return redirect("/")
