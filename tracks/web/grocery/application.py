9362e1cff2f9a20b7f8b3fab4a89beaf90e18759
Tangue Muweh <atmuweh@gmail.com>
2020-09-29 22:19:07 +0000

    add session

--- tracks/web/grocery/application.py
+++ tracks/web/grocery/application.py
@@ -1,13 +1,17 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session
+from flask_session import Session
 
 app = Flask(__name__)
-
-#list of grocies
-grocies = []
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html" , grocies=grocies)
+    # create session variable grocies to store list of grocies
+    if "grocies" not in session:
+        session["grocies"] = []
+    return render_template("index.html" , grocies=session["grocies"])
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
@@ -15,5 +19,5 @@ def add():
         return render_template("add.html")
     else:
         name = request.form.get("name")
-        grocies.append(name)
+        session["grocies"].append(name)
         return redirect("/")
