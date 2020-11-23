85c99f94810168d95d95afa9216d201c7a420fe5
Tangue Muweh <atmuweh@gmail.com>
2020-11-23 23:23:25 +0000

    log in and sign up logic

--- project/nagpris/application.py
+++ project/nagpris/application.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -34,16 +35,31 @@ def index():
 
     return render_template("index.html")
 
+@app.route("/logout")
+def logout():
+
+    # forget user_id
+    session.clear()
+
+    return redirect("/")
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         first_name = request.form.get("first_name")
         last_name = request.form.get("last_name")
+        email = request.form.get("email")
         username = request.form.get("username")
-        password
-        pass
+        password = request.form.get("password")
+
+        # register to db
+        rows = db.execute("INSERT INTO users(username, first_name, last_name, hash, email) VALUES(:username, :first_name,:last_name, :hash, :email)",
+                                        username=username, first_name=first_name, last_name=last_name, hash=generate_password_hash("password"), email=email)
+        return redirect("/login")
     else:
         return render_template("register.html")
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -51,12 +67,22 @@ def login():
     session.clear()
     if request.method == "POST":
         username = request.form.get("username")
-        password = request.form.get("password")
-        
-        #hash = check_password_hash(password)
+
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = :username",
                           username=request.form.get("username"))
-        pass
+        # Ensure username exists and password is correct
+        print(len(rows))
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+
+            return render_template("login.html", message="Invalid username/password")
+
+        else:
+            # Remember which user has logged in
+            session["user_id"] = rows[0]["user_id"]
+            session["username"] = rows[0]["username"]
+
+            # Redirect user to home page
+            return redirect("/")
     else:
         return render_template("login.html")
